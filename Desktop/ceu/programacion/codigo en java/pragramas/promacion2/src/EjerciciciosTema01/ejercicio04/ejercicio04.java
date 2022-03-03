5c290c36677112ac98d931b63eacc84f280ac746
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-03 09:21:16 +0100

    ejercicosTema01

---
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio04/ejercicio04.java
@@ -0,0 +1,43 @@
+package EjerciciciosTema01.ejercicio04;
+
+import java.util.ArrayList;
+import java.util.List;
+import utilidades.utilidades;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class ejercicio04 {
+
+    public static void main(String[] args) {
+
+        List<Double> t = new ArrayList<Double>();
+        int[] ArrayList = null;
+        for (int i = 0; i <= 100; i++) {
+
+            ArrayList[i] = utilidades.leeEntero("introduce  numero en posicion " + i + " del array: ");
+            if (ArrayList[i] <= 0) {
+                double max = 0;
+                for (int j = 0; j < t.size(); j++) {
+                    if (t.get(j) > max) {
+                        max = t.get(j);
+                    }
+                }
+                double min = max;
+                for (int k = 0; k < t.size(); k++) {
+                    if (t.get(k) < min) {
+                        min = t.get(k);
+                    }
+                }
+                System.out.println("Máximo: " + max);
+                System.out.println("Mínimo: " + min);
+
+            } else {
+
+            }
+        }
+
+    }
+
+}
