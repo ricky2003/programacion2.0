f03d804259b870986f9b87e2b435958b0fed6849
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-04 10:27:59 +0100

    ejercicosTema01

---
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio13/EjercicioParaEntregar13.java
@@ -0,0 +1,36 @@
+package EjerciciciosTema01.ejercicio13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class EjercicioParaEntregar13 {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Escribe una cadena de texto :");
+            String palabra;
+            String palabra_invertida = "";
+
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            palabra = consola.readLine();
+
+            // Recorremos la cadena original del final al inicio
+            for (int i = palabra.length() - 1; i >= 0; i--) {
+                //el operador += sirve para concatenar cadenas
+                palabra_invertida += palabra.charAt(i);
+            }
+            System.out.println("la cadena al derecho es : " + palabra);
+            System.out.println("la cadena al reves es: " + palabra_invertida);
+
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error" + ex);
+        }
+    }
+
+}
