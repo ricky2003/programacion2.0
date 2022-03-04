b2af56c642e8a3b0648bff996cd028013fe4d6fa
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-04 09:49:49 +0100

    ejercicosTema01

---
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio12/ejercicio12ParaEntregar.java
@@ -0,0 +1,40 @@
+package EjerciciciosTema01.ejercicio12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class ejercicio12ParaEntregar {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+
+            System.out.println("Escribe una cadena de texto :");
+            String palabra;
+            int contador_vocales = 0;
+            int contador_consonantes = 0;
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            palabra = consola.readLine();
+            System.out.println("La cadena de texto leida es:" + palabra);
+            for (int i = 0; i <= palabra.length() - 1; i++) {
+                if (palabra.charAt(i) == 'a' || palabra.charAt(i) == 'e' || palabra.charAt(i) == 'i' || palabra.charAt(i) == 'o' || palabra.charAt(i) == 'u') {
+                    contador_vocales++;
+
+                } else {
+                    contador_consonantes++;
+
+                }
+            }
+            System.out.println("la cantida de vocales son: " + contador_vocales);
+            System.out.println("la cantidad de consonantes es: " + contador_consonantes);
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error" + ex);
+        }
+    }
+
+}
