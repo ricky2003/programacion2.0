917f44298017bc0810195b760da6e87059eae609
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-04 11:39:52 +0100

    ejercicosTema01

--- Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio06/ejercicioparaentregar06.java
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio06/ejercicioparaentregar06.java
@@ -6,24 +6,63 @@ package EjerciciciosTema01.ejercicio06;
  */
 public class ejercicioparaentregar06 {
 
-    public static void main(String[] args) {
-        boolean numero;
+    public static int esPrimo(int Primo) {
+        boolean numero = true;
+
+        for (int j = 2; j > 1; j--) {
+            //si si es primo es divisible entre 2 este no se te pondra en la pantalla
+            if (Primo % j == 0) {
+
+                numero = false;
+                break;
+            }
+        }
+        //este bucle te ense;a todos los primos del 1 al 100
+        if (numero == true) {
+            System.out.println("el numero " + Primo + " es primo");
+        }
+        return Primo;
+    }
+
+    ;
+
+    public static boolean cienPrimos(boolean numero) {
+        int Primo = 0;
+
         System.out.println("Son números primos del 1 al 100:\n");
+
         //estructura para saber si es primo o no
-        for (int esPrimo = 2; esPrimo <= 100; esPrimo++) {
+        for (Primo = 2; Primo <= 100; Primo++) {
+
             numero = true;
+
             for (int j = 2; j > 1; j--) {
+
                 //si si es primo es divisible entre 2 este no se te pondra en la pantalla
-                if (esPrimo % j == 0) {
+                if (Primo % j == 0) {
+
                     numero = false;
                     break;
                 }
             }
+
             //este bucle te ense;a todos los primos del 1 al 100
             if (numero == true) {
-                System.out.println("el numero " + esPrimo + " es primo");
+
+                System.out.println("el numero " + Primo + " es primo");
             }
         }
+
+        return numero;
+    }
+
+    ;
+
+    public static void main(String[] args) {
+
+        boolean numero = true;
+
+        ejercicioparaentregar06.cienPrimos(numero);
 
     }
 
