a87ff7597d2fda99d647bacefb309b58c8b37c49
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-04 10:57:06 +0100

    ejercicosTema01

---
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio14/EjercicioParaEntregar14.java
@@ -0,0 +1,36 @@
+package EjerciciciosTema01.ejercicio14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class EjercicioParaEntregar14 {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Escribe una cadena de texto :");
+            String palabra;
+            String palabra_invertida = "";
+
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            palabra = consola.readLine();
+
+            for (int i = palabra.length() - 1; i >= 0; i--) {
+                palabra_invertida += palabra.charAt(i);
+            }
+            if (palabra_invertida.equals(palabra)) {
+                System.out.println("la palabra es palindroma");
+            } else {
+                System.out.println("la palabra no es palindroma");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error" + ex);
+        }
+    }
+}
