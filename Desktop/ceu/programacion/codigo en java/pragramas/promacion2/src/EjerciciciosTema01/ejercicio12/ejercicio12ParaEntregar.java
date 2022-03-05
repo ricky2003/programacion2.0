45963f0c05fc90666022913fd1236bc0e0fdb6c5
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-03-05 20:04:55 +0100

    ejercicosTema01

--- Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio12/ejercicio12ParaEntregar.java
+++ Desktop/ceu/programacion/codigo en java/pragramas/promacion2/src/EjerciciciosTema01/ejercicio12/ejercicio12ParaEntregar.java
@@ -10,16 +10,18 @@ import java.io.InputStreamReader;
  */
 public class ejercicio12ParaEntregar {
 
-    public static String cantidadvocales(String palabra) {
+    public static String LeeUnString(String palabra) {
+
         try {
+            int contador_vocales = 0;
+            int contador_consonantes = 0;
 
             System.out.println("Escribe una cadena de texto :");
 
-            int contador_vocales = 0;
-            int contador_consonantes = 0;
             BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
             palabra = consola.readLine();
             System.out.println("La cadena de texto leida es:" + palabra);
+
             for (int i = 0; i <= palabra.length() - 1; i++) {
 
                 if (palabra.charAt(i) == 'a' || palabra.charAt(i) == 'e' || palabra.charAt(i) == 'i' || palabra.charAt(i) == 'o' || palabra.charAt(i) == 'u') {
@@ -32,17 +34,20 @@ public class ejercicio12ParaEntregar {
             }
             System.out.println("la cantida de vocales son: " + contador_vocales);
             System.out.println("la cantidad de consonantes es: " + contador_consonantes);
+
+            return palabra;
+
         } catch (IOException ex) {
-            System.out.println("Se ha producido un error" + ex);
+            System.out.println("ha habido un error");
         }
         return palabra;
     }
 
     ;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String palabra = "";
-        ejercicio12ParaEntregar.cantidadvocales(palabra);
+        ejercicio12ParaEntregar.LeeUnString(palabra);
 
     }
 
