c55dfb836803d1a73638d21855efbc8c6457e1bb
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2022-04-16 17:34:54 +0200

    ejercicosTema01

---
+++ NetBeansProjects/RGarciaVillaracoS/src/practicaprograii/PracticaPrograII.java
@@ -0,0 +1,16 @@
+package practicaprograii;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class PracticaPrograII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
