c77a9c65ce6ce43bff951daaac50eb8590ecfdc6
ricardogarciavillaraco <ricardogarciavillaraco@192.168.1.32>
2023-03-25 18:07:21 +0100

    ejerciciosTema1

---
+++ NetBeansProjects/new/src/pkgnew/New.java
@@ -0,0 +1,16 @@
+package pkgnew;
+
+/**
+ *
+ * @author Ricardo García-Villaraco<ricardo.garciavillaracosanchez@usp.ceu.es>
+ */
+public class New {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+
+}
